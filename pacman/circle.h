2e00142a0f5ccf6be48c89c85b72d6f488235ee7
Zachary Steudel <zacshack174@gmail.com>
2017-11-14 17:49:21 -0600

    Added comments at top of file
    process of documentation begins now

--- pacman/circle.h
+++ pacman/circle.h
@@ -1,3 +1,14 @@
+/*
+* Author: Zachary Steudel, Chase Manseth, Meghan Bibb,
+* Will Rhoden, Bailey Stone
+* Assignment Title:  Pacman Group Project
+* Assignment Description: User can play a faithfully recreated
+* version of pacman.
+* Due Date: 11/29/2017
+* Date Created: 11/1/2017
+* Date Last Modified: 11/11/2017
+*/
+
 #ifndef CIRCLE_H_INCLUDED
 #define CIRCLE_H_INCLUDED
 
