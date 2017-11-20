a2e341e9cbbfc1ab5d0c1c3d121a624c80bc9cdb
SicEmBears21 <33154839+SicEmBears21@users.noreply.github.com>
2017-11-20 15:03:46 -0600

    function block comments

--- pacman/map.h
+++ pacman/map.h
@@ -8,9 +8,18 @@ using namespace std;
 
 struct Map{
     int tMap[36][28];
+
+    /*
+    * Description: Constructor, reads and assigns the
+    *              values of "mapText.txt" to tMap
+    * Return: none
+    * Precondition: the file, "mapText.txt", exists
+    * Postcondition: tMap holds the location of the map tiles
+    */
     Map(){
         ifstream input;
         input.open("mapText.txt");
+
         for(int i = 0; i < 36; i++){
             for(int k = 0; k < 28; k++){
                 input >> tMap[i][k];
@@ -18,7 +27,6 @@ struct Map{
             }
             cout << endl;
         }
-
     }
 
 };
