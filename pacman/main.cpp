471f00c387f00fa3dc11639a68ced3fa8080024d
Zachary Steudel <zacshack174@gmail.com>
2017-12-02 15:25:03 -0600

    main is completely documented

--- pacman/main.cpp
+++ pacman/main.cpp
@@ -4,11 +4,38 @@
 * Assignment Title:  Pacman Group Project
 * Assignment Description: User can play a faithfully recreated
 * version of pacman.
-* Due Date: 11/29/2017
+* Due Date: 12/4/2017
 * Date Created: 11/1/2017
-* Date Last Modified: 11/11/2017
+* Date Last Modified: 12/2/2017
 */
-// -lmingw32 -lSDL2main -lSDL2 -lSDL2_mixer
+
+/*
+Data Abstraction:
+    7 Mix_Chunks to hold the sound files used in the game.
+    int nums[][][], lets[][][], and graphic[][][] to hold font and graphical values.
+    bool releaseCalled for checking if a ghost has been released from box.
+    bool rando for sending in to releaseGhost.
+    int numEaten for counting the number of pellets eaten.
+    int counter to start a timer when an energizer is eaten.
+    SDL_Plotter g for holding the game window and graphics.
+    Pacman pac to hold the pacman object that the user plays as.
+    Tile map[][] to hold every tile of the game.
+    color blink, pink, ink, cly for the colors of each respective ghost.
+    ghosts inky, blinky, pinky and clyde for each ghost object.
+
+Input: User can input directional keys in order to move
+pacman around the game map.
+
+Process: Game handles pacman movement and ghost movement
+in order to have ghosts chase pacman while the user moves pacman in normal state.
+When in scared state, the ghosts will run away from pacman as the user chases them.
+
+Output: program displays pacman game through SDL_Plotter window.
+program outputs pacman and ghost movement, score updating, and
+various uses of font to print messages to the user.
+
+Assumptions: no assumptions are made.
+*/
 #include <iostream>
 #include <fstream>
 #include "pacman.h"
@@ -19,11 +46,25 @@
 
 using namespace std;
 
+/*
+Description: function resets the ghosts into
+their monster box.
+Return: none
+Precondition: all ghost objects are valid, g and map are valid.
+Postcondition: ghost positions are moved.
+*/
 void rePositionGhosts(SDL_Plotter& g, Ghost& a, Ghost& b, Ghost& c, Ghost& d, Tile map[36][28]);
+/*
+Description: function releases one ghost from monster box
+Return: none
+Precondition: ghost object, map, and bool r are valid.
+Postcondition: ghost position is changed and ghost is set to active.
+*/
 void releaseGhost(Ghost& a, Tile map[36][28], bool& r);
 
 int main(int argc, char** argv)
 {
+    //data abstraction
     Mix_Chunk *gEat         = NULL;
     Mix_Music *gAmbient     = NULL;
     Mix_Chunk *gDeath       = NULL;
@@ -31,13 +72,34 @@ int main(int argc, char** argv)
     Mix_Chunk *gPowerPellet = NULL;
     Mix_Chunk *gGame_Over   = NULL;
     Mix_Chunk *gBegin       = NULL;
+    char key;
+    // Graphic Arrays
+    int nums[10][25][25];
+    int lets[26][25][25];
+    int graphic[31][25][25];
+
     bool releaseCalled = false;
     bool rando    = false;
-    int numEaten  = 1;
-
+    int numEaten  = 0;
     int counter   = 0;
-    int prevScore = 0;
     SDL_Plotter g(900,700);
+
+    Pacman pac(26,13);
+    Tile map[36][28];
+    // Initialize map and everything in it
+    init(map, g, nums, lets, graphic);
+
+    Color blink(255,20,20);
+    Ghost blinky(blink, map);
+
+    Color pink(255,102,178);
+    Ghost pinky(pink, map);
+
+    Color ink(0, 255, 255);
+    Ghost inky(ink, map);
+
+    Color cly(255, 165, 0);
+    Ghost clyde(cly, map);
 
     //initialize audio functionality
     SDL_Init(SDL_INIT_AUDIO);
@@ -52,49 +114,26 @@ int main(int argc, char** argv)
     gGame_Over = Mix_LoadWAV("game_over.wav");
     gBegin = Mix_LoadWAV("pacman_beginning.wav");
 
-    Tile map[36][28];
-
-     // Graphic Arrays
-    int nums[10][25][25];
-    int lets[26][25][25];
-    int graphic[31][25][25];
-    // Init
-    init(map, g, nums, lets, graphic);
-
-    //prints score
+    //process & output to screen
+    //prints score, initializes numbers and font
     initLetter();
+    initNumber();
+
     drawLetter(g, 2, 1, s);
     drawLetter(g, 3, 1, c);
     drawLetter(g, 4, 1, o);
     drawLetter(g, 5, 1, r);
     drawLetter(g, 6, 1, e);
 
-    initNumber();
-    Pacman pac(26,13);
     updateScore(pac.getScore(), g, map);
 
-
-    Color blink(255,20,20);
-    Ghost Blinky(blink, map);
-
-    Color pink(255,102,178);
-    Ghost pinky(pink, map);
-
-    Color ink(0, 255, 255);
-    Ghost inky(ink, map);
-
-    Color cly(255, 165, 0);
-    Ghost clyde(cly, map);
-
-    rePositionGhosts(g, Blinky, pinky, inky, clyde, map);
+    rePositionGhosts(g, blinky, pinky, inky, clyde, map);
 
     pac.drawPac(g);
 
-    char key;
-
     pinky.draw(g, map);
     inky.draw(g, map);
-    Blinky.draw(g, map);
+    blinky.draw(g, map);
     clyde.draw(g, map);
 
     g.update();
@@ -102,30 +141,44 @@ int main(int argc, char** argv)
     Mix_PlayChannel( -1, gBegin, 0 );
     g.Sleep(4000);
 
+    //ambient ghost noise in background
     Mix_PlayMusic(gAmbient, -1 );
 
     Mix_VolumeMusic(24);
 
+    //master loop of game
     while(!g.getQuit())
     {
         for(int i = 0; i < 60; i++)
         {
-            if(numEaten == 245)
+            //maximum number of pellets, this signifies a win
+            if(numEaten == 244)
             {
                 //you win
                 Mix_PauseMusic();
+                //make temporary score and lives int to hold previous pacman data
                 int score = pac.getScore();
+                int lives = pac.getLives();
+                //make new pacman
                 pac = Pacman(26, 13);
                 init(map, g, nums, lets, graphic);
                 pac.setScore(score);
+                pac.setLives(lives + 1);
+
+                pac.setState(NORMAL);
 
                 pac.drawPac(g);
 
-                rePositionGhosts(g, Blinky, inky, pinky, clyde, map);
+                rePositionGhosts(g, blinky, inky, pinky, clyde, map);
+
+                blinky.setColor(blink);
+                inky.setColor(ink);
+                pinky.setColor(pink);
+                clyde.setColor(cly);
 
                 pinky.draw(g, map);
                 inky.draw(g, map);
-                Blinky.draw(g, map);
+                blinky.draw(g, map);
                 clyde.draw(g, map);
 
                 drawLetter(g, 2, 1, s);
@@ -139,34 +192,39 @@ int main(int argc, char** argv)
                 Mix_PlayChannel( -1, gBegin, 0 );
                 g.Sleep(4000);
 
-                numEaten = 1;
+                numEaten = 0;
 
                 Mix_ResumeMusic();
             }
             map[35][0].drawTile(g);
             map[35][1].drawTile(g);
             map[35][2].drawTile(g);
+            map[35][3].drawTile(g);
+            map[35][4].drawTile(g);
+            map[35][5].drawTile(g);
 
-            for(int z = 0; z < pac.getLives(); z++)
+            //print the lives of pacman at bottom left
+            for(int c = 0; c < pac.getLives(); c++)
             {
-                Pacman p = Pacman(35, z);
+                Pacman p = Pacman(35, c);
+                p.setWaka(FULL_WAKA);
                 p.drawPac(g);
             }
 
             //handle pacman animation
-            if(pac.getWaka() == HALF2 && i % 10 == 0)
+            if(pac.getWaka() == HALF2 && i % 8 == 0)
             {
                 pac.setWaka(CLOSED_WAKA);
             }
-            else if(i % 10 == 0)
+            else if(i % 8 == 0)
             {
                 pac.setWaka(pac.getWaka() + 1);
             }
             //erase the old position of ghosts
             pinky.erase(g, map);
             map[pinky.getR()][pinky.getC()].drawTile(g);
-            Blinky.erase(g, map);
-            map[Blinky.getR()][Blinky.getC()].drawTile(g);
+            blinky.erase(g, map);
+            map[blinky.getR()][blinky.getC()].drawTile(g);
             inky.erase(g, map);
             map[inky.getR()][inky.getC()].drawTile(g);
             clyde.erase(g, map);
@@ -174,7 +232,7 @@ int main(int argc, char** argv)
 
             if(pac.getState() == NORMAL)
             {
-                //handle ghost movement & collision
+                //handles ghost movement & collision
                 //+ pacman death, death sounds
                 pinky.target(pac);
                 //pinky is slowest
@@ -183,74 +241,35 @@ int main(int argc, char** argv)
                     pinky.move(map, g);
                 }
                 pinky.draw(g, map);
-                if(pinky.isCollide(map, pac, g))
-                {
-                    Mix_PauseMusic();
-                    Mix_PlayChannel( -1, gDeath, 0 );
-                    g.Sleep(1500);
-                    Mix_ResumeMusic();
-                    rePositionGhosts(g, Blinky, pinky, inky, clyde, map);
-                    pac.setR(26);
-                    pac.setC(13);
-
-                    pac.setLives(pac.getLives() - 1);
-
-                    pac.setCenter(map[26][13].getCenter());
-                }
-                Blinky.target(pac);
+                blinky.target(pac);
                 //blinky is fastest
                 if(i % 3 == 0)
                 {
-                    Blinky.move(map, g);
+                    blinky.move(map, g);
                 }
-                Blinky.move(map, g);
-                Blinky.draw(g, map);
-                if(Blinky.isCollide(map, pac, g))
-                {
-                    Mix_PauseMusic();
-                    Mix_PlayChannel( -1, gDeath, 0 );
-                    g.Sleep(1500);
-                    Mix_ResumeMusic();
-                    rePositionGhosts(g, Blinky, pinky, inky, clyde, map);
-                    pac.setR(26);
-                    pac.setC(13);
-
-                    pac.setLives(pac.getLives() - 1);
-
-                    pac.setCenter(map[26][13].getCenter());
-                }
+                blinky.move(map, g);
+                blinky.draw(g, map);
                 inky.target(pac);
                 //inky is normal speed
                 inky.move(map, g);
                 inky.draw(g, map);
-                if(inky.isCollide(map, pac, g))
-                {
-                    Mix_PauseMusic();
-                    Mix_PlayChannel( -1, gDeath, 0 );
-                    g.Sleep(1500);
-                    Mix_ResumeMusic();
-                    rePositionGhosts(g, Blinky, pinky, inky, clyde, map);
-                    pac.setR(26);
-                    pac.setC(13);
-
-                    pac.setLives(pac.getLives() - 1);
-
-                    pac.setCenter(map[26][13].getCenter());
-                }
                 clyde.target(pac);
+                //clyde is second fastest
                 if(i % 5 == 0)
                 {
                     clyde.move(map, g);
                 }
                 clyde.move(map, g);
                 clyde.draw(g, map);
-                if(clyde.isCollide(map, pac, g))
+                //check collision with any ghosts
+                if(clyde.isCollide(map, pac, g) || inky.isCollide(map, pac, g)
+                   || blinky.isCollide(map, pac, g) || pinky.isCollide(map, pac, g))
                 {
                     Mix_PauseMusic();
                     Mix_PlayChannel( -1, gDeath, 0 );
                     g.Sleep(1500);
                     Mix_ResumeMusic();
-                    rePositionGhosts(g, Blinky, pinky, inky, clyde, map);
+                    rePositionGhosts(g, blinky, pinky, inky, clyde, map);
                     pac.setR(26);
                     pac.setC(13);
 
@@ -273,6 +292,8 @@ int main(int argc, char** argv)
                     drawLetter(g, 4, 1, o);
                     drawLetter(g, 5, 1, r);
                     drawLetter(g, 6, 1, e);
+
+                    numEaten = 0;
                 }
             }
             else if(pac.getState() == SUPERPAC)
@@ -283,21 +304,22 @@ int main(int argc, char** argv)
                 }
 
                 //set ghosts dark blue
-                Blinky.setColor(Color(0, 0, 205));
+                blinky.setColor(Color(0, 0, 205));
                 inky.setColor(Color(0, 0, 205));
                 pinky.setColor(Color(0, 0, 205));
                 clyde.setColor(Color(0, 0, 205));
 
-                Blinky.target(pac);
+                blinky.target(pac);
                 inky.target(pac);
                 pinky.target(pac);
                 clyde.target(pac);
 
+                //all ghosts are now slower
                 if(i % 8 == 0)
                 {
-                    Blinky.moveFrightened(map, g, pac);
+                    blinky.moveFrightened(map, g, pac);
                 }
-                Blinky.moveFrightened(map, g, pac);
+                blinky.moveFrightened(map, g, pac);
                 if(i % 2 == 0)
                 {
                     pinky.moveFrightened(map, g, pac);
@@ -310,11 +332,11 @@ int main(int argc, char** argv)
                 clyde.moveFrightened(map, g, pac);
                 //check for ghost dying, play death noise
                 //increment score
-                if(Blinky.isCollide(map, pac, g))
+                if(blinky.isCollide(map, pac, g))
                 {
                     Mix_PlayChannel( -1, gGhostDead, 0 );
                     g.Sleep(250);
-                    releaseGhost(Blinky, map, rando);
+                    releaseGhost(blinky, map, rando);
                     pac.setScore(pac.getScore() + 200);
 
                     updateScore(pac.getScore(), g, map);
@@ -352,7 +374,7 @@ int main(int argc, char** argv)
 
                 }
 
-                Blinky.draw(g, map);
+                blinky.draw(g, map);
                 inky.draw(g, map);
                 pinky.draw(g, map);
                 clyde.draw(g, map);
@@ -362,7 +384,7 @@ int main(int argc, char** argv)
                 {
                     pac.setState(NORMAL);
 
-                    Blinky.setColor(blink);
+                    blinky.setColor(blink);
                     inky.setColor(ink);
                     pinky.setColor(pink);
                     clyde.setColor(cly);
@@ -390,10 +412,10 @@ int main(int argc, char** argv)
             //releasing ghosts based on pellet eating
             if(numEaten % 5 == 0 && !releaseCalled && numEaten != 0)
             {
-                if(!Blinky.getActive())
+                if(!blinky.getActive())
                 {
-                    map[Blinky.getR()][Blinky.getC()].drawTile(g);
-                    releaseGhost(Blinky, map, releaseCalled);
+                    map[blinky.getR()][blinky.getC()].drawTile(g);
+                    releaseGhost(blinky, map, releaseCalled);
                 }
                 else if(!inky.getActive())
                 {
@@ -415,12 +437,9 @@ int main(int argc, char** argv)
             pac.drawPac(g);
             g.update();
 
-
-
             if(g.kbhit())
             {
                 key = g.getKey();
-                pac.erasePac(g);
 
                 switch(key)
                 {
@@ -429,37 +448,32 @@ int main(int argc, char** argv)
                                             pac.setDirection(RIGHT);
                                       }
                                       pac.setNextDir(RIGHT);
-                                      pac.movePosition(pac.getDirection(), map, g);
                                       break;
                     case LEFT_ARROW:  if(map[pac.getR()][pac.getC() - 1].isPath() && abs(pac.getCenter().y - (pac.getR() * 25 + 12)) <= 4)
                                       {
                                             pac.setDirection(LEFT);
                                       }
                                       pac.setNextDir(LEFT);
-                                      pac.movePosition(pac.getDirection(), map, g);
                                       break;
                     case UP_ARROW:    if(map[pac.getR() - 1][pac.getC()].isPath() && abs(pac.getCenter().x - (pac.getC() * 25 + 12)) <= 4)
                                       {
                                             pac.setDirection(UP);
                                       }
                                       pac.setNextDir(UP);
-                                      pac.movePosition(pac.getDirection(), map, g);
                                       break;
                     case DOWN_ARROW:  if(map[pac.getR() + 1][pac.getC()].isPath() && abs(pac.getCenter().x - (pac.getC() * 25 + 12)) <= 4)
                                       {
                                             pac.setDirection(DOWN);
                                       }
                                       pac.setNextDir(DOWN);
-                                      pac.movePosition(pac.getDirection(), map, g);
                                       break;
                 }
 
             }
-            else
-            {
-                pac.erasePac(g);
-                pac.movePosition(pac.getDirection(), map, g);
-            }
+
+            pac.erasePac(g);
+            pac.movePosition(pac.getDirection(), map, g);
+
             //dictates pacman speed, adds extra moving
             if(i % 2 == 0)
             {
